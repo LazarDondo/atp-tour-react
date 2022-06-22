62650817b7aeeef89ca9495c2315b2a57022d4f7
Lazar Dondo <ldondo@s4hc.com>
2022-06-22 20:22:17 +0200

    init

---
+++ atp-tour/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch, BrowserRouter } from "react-router";
+import Login from './login/Login';
+import './App.css';
+
+const App = ()=>{
+  return (
+    <div>
+      App
+    </div>
+  );
+}
+
+export default App;
