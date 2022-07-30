8b67cfa95cdee64998f207cb64051d9c18638018
Lazar Dondo <ldondo@s4hc.com>
2022-07-30 16:49:31 +0200

    Change input value for FormInput

--- atp-tour/src/components/form/FormInput.js
+++ atp-tour/src/components/form/FormInput.js
@@ -4,11 +4,19 @@ import './FormInput.css';
 
 class FormInput extends React.Component {
 
+    lostFocus = false;
+
+    onBlur = () => {
+       this.lostFocus=true;
+    }
+
+    
     renderField = ({ label, input, type, name, className }) => {
+        const inputValue = this.props.value;
         return (
             <>
                 <label htmlFor={name}>{label}</label>
-                <input {...input} value={input.value} type={type} name={name} className={className} />
+                <input {...input} value={inputValue} type={type} name={input.name} className={className} onChange={input.onChange} readOnly={this.props.readOnly} />
             </>
         );
     }
@@ -23,10 +31,11 @@ class FormInput extends React.Component {
     }
 
     render() {
-        const { name, type, fieldValidation, label } = this.props;
+        const { name, type, fieldValidation, label, onChange } = this.props;
+
         return (
             <div className='form-group'>
-                <Field name={name} component={this.renderField} type={type}
+                <Field name={name} component={this.renderField} type={type} onChange={onChange}
                     className={'form-control ' + (fieldValidation ? 'is-invalid' : '')} label={label} />
                 {this.renderError()}
             </div>
